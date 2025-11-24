e634c92b4fc09e0896e0f4289455937529d087c5
Cesar Filho <cesarscfilho@gmail.com>
2025-11-24 20:42:06 -0300

    feat: Update partner schema

---
+++ drizzle/0001_slow_the_hunter.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "professionals" DROP COLUMN "expertise";--> statement-breakpoint
+ALTER TABLE "professionals" DROP COLUMN "phone_verified_at";
--- src/db/schema/partners.ts
+++ src/db/schema/partners.ts
@@ -1,13 +1,11 @@
-import { pgTable, text, timestamp, uuid, varchar } from "drizzle-orm/pg-core"
+import { pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core"
 import { lifecycleDates } from "../utils"
 
 export const partners = pgTable("professionals", {
   id: uuid("id").primaryKey().defaultRandom(),
+  phone: varchar("phone", { length: 20 }).notNull().unique(),
   name: varchar("name", { length: 255 }).notNull(),
   email: varchar("email", { length: 255 }).unique(),
-  phone: varchar("phone", { length: 20 }).notNull().unique(),
-  expertise: text("expertise"),
-  phoneVerifiedAt: timestamp("phone_verified_at"),
   emailVerifiedAt: timestamp("email_verified_at"),
   ...lifecycleDates,
 })
